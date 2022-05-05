7fdf8d2463acd7b90f9492854e3cf9795e84f4e4
Marcelo Peixoto de Abreu <marcelopeixotodeabreu@gmail.com>
2022-05-05 09:58:07 -0300

    Criando o projeti easy-bill

---
+++ src/main/java/br/com/oobj/easybill/EasyBillApplication.java
@@ -0,0 +1,13 @@
+package br.com.oobj.easybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oobj/easybill/EasyBillApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oobj.easybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
